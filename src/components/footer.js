bd2c6e99c60bedabd7c710c4fb30cadf06d50c64
joematte <mattesoj@augsburg.edu>
2021-10-26 22:43:14 -0500

    more apps

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,6 +5,10 @@ import { faTwitter, faDiscord } from "@fortawesome/free-brands-svg-icons";
 export default function Footer() {
   return (
     <div>
+      <div className="flex flex-col items-center justify-center">
+        <p>Have an app not listed?</p>
+        <p>Join our discord and suggest it.</p>
+      </div>
       <div className="w-full h-20 flex justify-center items-center text-2xl">
         <a
           className="px-2 transition ease-in-out duration-300 rounded leading-snug text-center text-primary hover:text-secondary"
